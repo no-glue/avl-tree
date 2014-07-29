0fb45fa355bb900bbc057c942b1a347845680460
no-glue <nikolapav1985@gmail.com>
2014-07-30 00:27:15 +0200

    rotating right

--- avl_node.py
+++ avl_node.py
@@ -1,21 +1,38 @@
 class AvlNode:
   def __init__(self, key, value, height = 0, left = None, right = None):
     """Set key and value for the node"""
-    self.key = key
-    self.value = value
-    self.height = height
-    self.left = left
-    self.right = right
-  def left(self):
-    """Return left subtree"""
-    return self.left
-  def right(self):
-    """Return right subtree"""
-    return self.right
-  def height(self):
-    """Return own height"""
-    return self.height
-  def height(self, height):
-    """Set own height"""
-    self.height = height
-    return self
+    self._key = key
+    self._value = value
+    self._height = height
+    self._left = left
+    self._right = right
+    @property
+    def key(self):
+        return self._key
+    @key.setter
+    def key(self, key):
+        self._key = key
+    @property
+    def left(self):
+      """Return left subtree"""
+      return self._left
+    @left.setter
+    def left(self, node):
+      """Set the left node"""
+      self._left = node
+    @property
+    def right(self):
+      """Return right subtree"""
+      return self._right
+    @right.setter
+    def right(self, node):
+      """Set the right node"""
+      self._right = node
+    @property
+    def height(self):
+      """Return own height"""
+      return self._height
+    @height.setter
+    def height(self, height):
+      """Set own height"""
+      self._height = height
--- avl_tree.py
+++ avl_tree.py
@@ -9,6 +9,40 @@ class AvlTree:
     """Insert a node for real and give back root"""
     if not root:
       root = node
+    elif node.key < root.key:
+      root.left = self.insert(node, root.left)
+      if root.left.height - root.right.height == 2:
+        # Inserted node on the left side, check if left side is larger by 2
+        # this is not allowed
+        # at most 1 difference
+        if node.key < root.left.key:
+          root = self.rotateWithLeftChild(root)
+        else:
+          root = self.doubleWithLeftChild(root)
+          # It's in wrong position, put it on the right
+    elif node.key > root.key:
+      root.right = self.insert(node, root.right)
+      if root.right.height - root.left.height == 2:
+        # Inserted node on the right side, check if right side larger by 2
+        if node.key > root.right.key:
+          root = self.rotateWithRightChild(root)
+        else:
+          root = self.doubleWithRightChild(root)
+          # It's in wrong position, put it on the left
+    else:
+      # Duplicate happened, do nothing
+
     root.height = max(root.left.height, root.right.height) + 1
     # get root height, left or right subtree height + 1, depending which is greater
     return root
+  def rotateWithLeftChild(node):
+    """Rotate with left child"""
+    temp = node
+    # todo why the fuck i need temp?
+    return_node = node.left
+    temp.left = return_node.right
+
+    return_node.right = temp
+
+    # rotate to right
+    return return_node
